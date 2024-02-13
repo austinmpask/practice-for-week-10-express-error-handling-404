5ca6549f3589993c20640de9f4a7aa74772715fd
austinmpask <austinmpask@gmail.com>
2024-02-12 16:32:45 -0800

    done

--- server/app.js
+++ server/app.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('GET / This is the root URL');
+app.get("/", (req, res) => {
+  res.send("GET / This is the root URL");
 });
 
+function errorCatch(req, res, next) {
+  const error = new Error("Sorry, the requested resource couldn't be found");
+  error.statusCode = 404;
+  next(error);
+}
+
+function err(err, req, res, next) {
+  console.error(err);
+  const statusCode = err.statusCode || 500;
+  const response = {
+    message: err.message,
+    status: statusCode,
+  };
+  res.status(statusCode).json(response);
+}
+app.use(errorCatch);
+app.use(err);
+
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
